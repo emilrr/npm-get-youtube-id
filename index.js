8743a2f5117ece8f386063fbf74fa895629bb7b5
emilrr <emil.ruzhenov@gmail.com>
2016-12-01 12:37:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = function(url) {
+    if (url !== undefined) {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
+
+        if (match && match[2].length == 11) {
+            return match[2];
+        } else {
+            return null;
+        }
+    }
+
+    return null;
+}
